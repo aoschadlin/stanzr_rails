5777f3abdc11b520022fc2cc81b32210dfb74a07
Andre Oschadlin <andreoschadlin@gmail.com>
2011-10-21 12:38:29 -0700

    Initial creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stanzr::Application.config.secret_token = 'dd7b0221d6144ddf819055db24b7b3b3876e44af61cb1c36a77e98389cde067361eed79899b3e68ed699b607fc1b0a022c02c3d5e1a14c1584f89edcf52502f3'
